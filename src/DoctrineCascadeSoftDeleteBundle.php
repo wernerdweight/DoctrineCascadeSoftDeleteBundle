2eec0dd94f8b2a9ca20d4871a365535f8b55da82
Werner Dweight <wd@wernerdweight.com>
2019-10-21 08:52:51 +0200

    🐂 Initial commit

---
+++ src/DoctrineCascadeSoftDeleteBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace WernerDweight\DoctrineCascadeSoftDeleteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DoctrineCascadeSoftDeleteBundle extends Bundle
+{
+}
